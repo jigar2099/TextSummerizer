3860e98632144d33045dfc0423a570dc0f811870
jkb2099 <jigarbhanderi@gmail.com>
2026-03-20 10:49:51 +0100

    Logging,EnsureAnnotations,BoxConfig

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.textSummarizer.logging import logger
+
+logger.info("Starting text summarization process")
+logger.info("Loading text data from file: data.txt")
+logger.info("Summarizing text using TextRank algorithm")
+logger.info("Saving summary to file: summary.txt")
+logger.info("Text summarization process completed successfully")
